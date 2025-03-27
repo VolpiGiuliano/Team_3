a66f5e5724a9ead09bc6f6f355708cbdb933e992
tranky07 <tranchitella.daniel@gmail.com>
2025-03-27 13:14:58 +0100

    codice per la generazione di un esempio e commenti

--- python_client/generazione_dati.py
+++ python_client/generazione_dati.py
@@ -4,22 +4,26 @@ import requests
 MODEL_NAME = "llama3"
 OLLAMA_URL = "http://ollama:11434"
 
-# Funzione per verificare se il modello è disponibile
+""" Funzione per verificare se il modello è disponibile.
+    Se il collegamento con ollama fallisce la funzione 
+    stampa l'errore e restituisce False
+"""
 def model_exists(model_name):
     try:
-        response = requests.get(f"{OLLAMA_URL}/api/tags")
-        if response.status_code == 200:
+        response = requests.get(f"{OLLAMA_URL}/api/tags")   # chiama l'api di ollama
+        if response.status_code == 200:                     # controlla che l'api risponde
             models = response.json().get("models", [])
-            return any(m["name"].startswith(model_name) for m in models)
+            return any(m["name"].startswith(model_name) for m in models) #controlla che il modello richiesto sia presente
         else:
             print(f"Errore nella richiesta: {response.status_code}")
             return False
-    except requests.exceptions.ConnectionError:
+    except requests.exceptions.ConnectionError:             # errore di connessione al docker ollama
         print("⚠️ Impossibile connettersi a Ollama. È in esecuzione?")
         return False
 
-# Prompt condizionale
+# Prompt per generazione condizionale
 def generate_prompt():
+    # definisce il parametro di contesto per la generazione condizionale
     context = """
     ### CONTEXT:
     Azienda: ABC Corp
@@ -36,17 +40,18 @@ def generate_prompt():
         "model": MODEL_NAME,
         "prompt": context,
         "options": {
-            "temperature": 0.8,
-            "top_p": 0.95,
-            "num_predict": 300,
-            "repeat_penalty": 1.2
+            "temperature": 0.8,     # definisce la creatività: 0.1 poco creativo
+            "top_p": 0.95,          # definisce i token per la generazione
+            "num_predict": 300,     # numero di token da generare: definiscono la lunghezza del documento
+            "repeat_penalty": 1.2   # evita le ripetizioni di parole nei documenti
         },
-        "stream": False
+        "stream": False             # la risposta è in un unico prompt
     }
 
     # Chiamata HTTP verso l'API di Ollama
-    response = requests.post(f"{OLLAMA_URL}/api/generate", json=payload)
+    response = requests.post(f"{OLLAMA_URL}/api/generate", json=payload) # sfrutta l'api per mandare la richiesta
      
+    # controllo di richiesta andata a buon fine
     if response.ok:
         return response.json()["response"]
     else:
