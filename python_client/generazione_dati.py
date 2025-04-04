62cd9bb03845492497de7b48b8740f77af9bd971
Alexpaso16 <74004997+Alexpaso16@users.noreply.github.com>
2025-04-04 10:16:58 +0200

    Add files via upload

--- python_client/generazione_dati.py
+++ python_client/generazione_dati.py
@@ -2,27 +2,10 @@ import os           # per il salvataggio nel volume
 import requests     # per il controllo dello stato di ollama
 from langchain_core.prompts import PromptTemplate   # per l'uso di promt strutturati
 from langchain_ollama import OllamaLLM              # integrazione ollama con langchain
+from langgraph.graph import Graph, START, END
 
 OLLAMA_URL = "http://ollama:11434"  # url api di ollama
 MODEL_NAME = "llama3"               # nome modello usato
-
-# 1. Verifica se il modello è installato
-def model_exists(model_name):
-    try:
-        response = requests.get(f"{OLLAMA_URL}/api/tags")
-        response.raise_for_status()
-        models = response.json().get("models", [])
-        return any(m["name"].startswith(model_name) for m in models)
-    except Exception as e:
-        print(f"Errore nella verifica del modello: {e}")
-        return False
-
-# 2. Installa il modello se non presente
-def pull_model(model_name):
-    print(f"📥 Scaricamento modello '{model_name}' da Ollama...")
-    response = requests.post(f"{OLLAMA_URL}/api/pull", json={"name": model_name}, stream=True)
-    for line in response.iter_lines():
-        print(line.decode())
 
 # inizializza l'agente
 def init_model():
@@ -31,17 +14,17 @@ def init_model():
         model=MODEL_NAME
     )
 
-# definisce le regole di generazione
-def get_prompt_template():
-    return PromptTemplate(
-        input_variables=["argomento"],  # definizione di variabile di prompt
-        template="Scrivi il seguente documento: {argomento}, tono professionale, lingua italiana."
-    )
-
 # definisce la catena di chiamata che deve effettuare l'agente al modello
-def run_chain(prompt, llm, input_data):
-    chain = prompt | llm
-    return chain.invoke(input_data)
+def ollama(input_data):
+    llm = init_model()
+    return llm.invoke(input_data)
+
+def constraints(input_data):
+    data = input_data.split("\n\n")
+    return data[1]
+
+def prompt():
+    return "Genera una tabella di 4 colonne nel seguente formato: |sesso|età|lavoro|entrate mensili|"
 
 # salva l'output in un file txt
 def salva_output(text, filename="outputs/output.txt"):
@@ -51,14 +34,20 @@ def salva_output(text, filename="outputs/output.txt"):
     print(f"\n💾 Output salvato in '{filename}'")
 
 def main():
-    if not model_exists(MODEL_NAME):
-        pull_model(MODEL_NAME)
 
-    llm = init_model()
-    prompt = get_prompt_template()
-    Argomento = "Estratto conto"    # input manuale che può essere sostituito da un inputs dinamico
-    input_data = {"argomento": Argomento}   # sostituisce i l'input alla parola argomento presente nel prompt
-    risposta = run_chain(prompt, llm, input_data)
+    workflow = Graph()
+
+    workflow.add_node("ollama", ollama)
+    workflow.add_node("constraints", constraints)
+
+    workflow.add_edge(START, "ollama")
+    workflow.add_edge("ollama", "constraints")
+    workflow.add_edge("constraints", END)
+
+    app = workflow.compile()
+
+    risposta = app.invoke(prompt())
+
     print("\n🧠 Risposta generata:\n")
     print(risposta)
     salva_output(risposta)
